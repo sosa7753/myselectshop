de8ebd902221aced25f5cb5b7382c734267534a5
sosa7753 <sosa7753@naver.com>
2025-02-04 22:30:40 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+
+}
---
+++ src/main/java/com/sparta/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+
+}
