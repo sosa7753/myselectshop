43bad7081161218ecb0b068fd1e6f60c05478acf
sosa7753 <sosa7753@naver.com>
2025-02-04 22:01:11 +0900

    회원별 상품 API 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.sparta.myselectshop.repository;
 
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+  List<Product> findAllByUser(User user);
 }
