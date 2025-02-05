49e98ebe5c5e3bded63c7a3279a5557e6ea7a6da
sosa7753 <sosa7753@naver.com>
2025-02-05 20:16:25 +0900

    Deprecated Exception 삭제

--- src/main/java/com/sparta/myselectshop/jwt/JwtUtil.java
+++ src/main/java/com/sparta/myselectshop/jwt/JwtUtil.java
@@ -1,18 +1,22 @@
 package com.sparta.myselectshop.jwt;
 
 import com.sparta.myselectshop.entity.UserRoleEnum;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-
-import java.security.Key;
-import java.util.Base64;
-import java.util.Date;
 
 @Slf4j(topic = "JwtUtil")
 @Component
@@ -65,7 +69,7 @@ public class JwtUtil {
     try {
       Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
       return true;
-    } catch (SecurityException | MalformedJwtException | SignatureException e) {
+    } catch (SecurityException | MalformedJwtException e) {
       log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
     } catch (ExpiredJwtException e) {
       log.error("Expired JWT token, 만료된 JWT token 입니다.");
